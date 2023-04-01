e655545cbe74f0474bf296b5a5b0d9d4b55496de
mikkmait <mikkmait@gmail.com>
2023-04-01 22:11:30 +0300

    Testing responsiveness

--- src/app/compnts/main/main.component.ts
+++ src/app/compnts/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main',
@@ -6,13 +7,56 @@ import { Component, ViewEncapsulation, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   private url: string = 'https://mikkmait-portfolio.herokuapp.com/articles';
 
   data: any;
+
+  xsmall = false;
+  small = false;
+  medium = false;
+  large = false;
+  xlarge = false;
+
+  constructor(private breakpointService: BreakpointObserver) {}
+
   ngOnInit(): void {
     fetch(this.url)
       .then((response) => response.json())
       .then((articles) => (this.data = articles));
+
+    this.breakpointService.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe((result) => {
+      this.small = false;
+      this.xsmall = false;
+      this.medium = false;
+      this.large = false;
+      this.xlarge = false;
+
+      if (result.breakpoints[Breakpoints.XSmall]) {
+        this.xsmall = true;
+      }
+
+      if (result.breakpoints[Breakpoints.Small]) {
+        this.small = true;
+      }
+
+      if (result.breakpoints[Breakpoints.Medium]) {
+        this.medium = true;
+      }
+
+      if (result.breakpoints[Breakpoints.Large]) {
+        this.large = true;
+      }
+
+      if (result.breakpoints[Breakpoints.XLarge]) {
+        this.xlarge = true;
+      }
+    })
   }
 }
